4dcfce2f6795b2d6d15991594a17b31069374789
Khai Pham <khaipham@gmail.com>
2025-04-22 01:58:30 -0400

    add testing using POM LoginPage

--- cypress/e2e/paylocity/login.cy.js
+++ cypress/e2e/paylocity/login.cy.js
@@ -1,29 +1,48 @@
 // cypress/e2e/paylocity/login.cy.js
+import { LoginPage } from '../pages/LoginPage';
+
+const loginPage = new LoginPage();
+describe('Paylocity login page ', () => {
+  beforeEach(() => {
+    cy.visit('https://access.paylocity.com');
+  });
+
+  it('should have valid page title and URL', () => {
+    cy.title().should('include', 'Paylocity');
+    cy.url().should('include', 'https://access.paylocity.com');
+  });
+
+  it('should display all necessary input fields and login button', () => {
+    cy.get('#CompanyId').should('be.visible').and('have.attr', 'name', 'CompanyId');
+    cy.get('#Username').should('be.visible').and('have.attr', 'name', 'Username');
+    cy.get('#Password').should('be.visible').and('have.attr', 'name', 'Password');
+    cy.get('.login-button').should('be.visible').and('contain.text', 'Login');
+  });
   
-  describe('Paylocity login page', () => {
-    beforeEach(() => {
-      cy.visit('https://access.paylocity.com');
-    });
-  
-    it('should have valid page title and URL', () => {
-      cy.title().should('include', 'Paylocity');
-      cy.url().should('include', 'https://access.paylocity.com');
+  it('should show an error message when login with empty fields', () => {
+    cy.get('.login-button').click();
+    cy.get('.banner-message.error').should('be.visible').invoke('text').then((text) => {
+      cy.log(`Banner message: ${text}`);
     });
 
-    it('should display all necessary input fields and login button', () => {
-      cy.get('#CompanyId').should('be.visible').and('have.attr', 'name', 'CompanyId');
-      cy.get('#Username').should('be.visible').and('have.attr', 'name', 'Username');
-      cy.get('#Password').should('be.visible').and('have.attr', 'name', 'Password');
-      cy.get('.login-button').should('be.visible').and('contain.text', 'Login');
-    });
-    
-    it('should show an error message when login with empty fields', () => {
-      cy.get('.login-button').click();
-      cy.get('.banner-message.error').should('be.visible').invoke('text').then((text) => {
-        cy.log(`Banner message: ${text}`);
-      });
+    // Debugging banner message:
+    // cy.get('.banner-message.error').debug();
 
-      // Validate the error message
-      cy.get('.banner-message.error').should('contain.text', 'The credentials provided are incorrect');
-    });
+    // Validate the error message
+    cy.get('.banner-message.error').should('contain.text', 'The credentials provided are incorrect');
+  });
+});
+
+describe('Paylocity Login - POM version', () => {
+  it('should show error on invalid login', () => {
+    loginPage.visit();
+    loginPage.enterCompanyId('123456');
+    loginPage.enterUsername('fakeuser');
+    loginPage.enterPassword('wrongpassword');
+    loginPage.clickLogin();
+
+    loginPage.getErrorMessage()
+      .should('be.visible')
+      .and('contain.text', 'The credentials provided are incorrect');
+  });
 });
--- cypress/e2e/paylocity/sso-login.cy.js
+++ cypress/e2e/paylocity/sso-login.cy.js
@@ -1,9 +1,13 @@
+// cypress/e2e/paylocity/sso-login.cy.js
+import { LoginPage } from '../pages/LoginPage';
+
+const loginPage = new LoginPage();
 
 describe('SSO Login page ', () => {
   beforeEach(() => {
     cy.visit('https://access.paylocity.com/Saml/SignIn');
   });
-it('should intercept SignIn API call and validate request responses', () => {
+  it('should return corresponding network request responses to invalid company', () => {
     cy.intercept('POST', '**/SignIn').as('loginRequest');
 
     cy.get('#CompanyId').type('abc123');
@@ -28,5 +32,24 @@ it('should intercept SignIn API call and validate request responses', () => {
       // Validate the SSO error message
       cy.get('.banner-message.error').should('contain.text', "This company doesn't support single sign-on");
     });
+  });
+  
+  // POP version of the login with SSO test
+  it('should return corresponding network request responses to invalid company - POM version', () => {
+    cy.intercept('POST', '**/SignIn').as('loginRequest');
+
+    loginPage.enterCompanyId('sso123');
+    loginPage.clickLoginSSO();
+    cy.wait('@loginRequest').then((interception) => {
+      const requestUrl = interception.request.url;
+      const statusCode = interception.response?.statusCode;
+      const rawBody = interception.request.body;
+      expect(statusCode).to.eq(200);
+      expect(requestUrl).to.include('https://access.paylocity.com/Saml/SignIn');
+      expect(rawBody).to.include('sso123');
+    });
+    loginPage.getErrorMessage()
+      .should('be.visible')
+      .and('contain.text', "This company doesn't support single sign-on");
   });
 });
