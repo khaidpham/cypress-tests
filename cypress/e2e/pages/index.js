3b2b78064e527ee26a63ab64b4909c21436170ba
Khai Pham <khaipham@gmail.com>
2025-04-22 02:16:48 -0400

    make cypress-tests available as an npm public package

---
+++ cypress/e2e/pages/index.js
@@ -0,0 +1,7 @@
+// index.js
+const { LoginPage } = require('./pages/LoginPage');
+
+module.exports = {
+  LoginPage
+};
+// This module exports the LoginPage class, which can be used in other test files.
